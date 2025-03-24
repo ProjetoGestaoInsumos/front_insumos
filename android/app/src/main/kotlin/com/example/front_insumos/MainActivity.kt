f3099bbdf57e859eb9731c3685402c418d27fcef
Eduardo Simas <eduardo.simas@fitmass.com.br>
2025-03-24 11:38:51 -0300

    Fist Commit

---
+++ android/app/src/main/kotlin/com/example/front_insumos/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.front_insumos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/utils/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static const Color white = Color(0xFFF2F2F2);
+}
